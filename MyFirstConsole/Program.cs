6dd8016f55147a01d1b85f0291d4f0de1ef46994
ShaaaNK <zohanztex@gmail.com>
2022-11-23 13:38:20 +0300

    add HM 2

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+var i = 10;
+Console.WriteLine($"Literal i = {i}");
+Console.ReadKey();
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+var pi = 3.141592653M;
+var e = 1.414213562373095048M;
+Console.WriteLine($"PI = {pi}\nPythagoras number = {e}");
+Console.ReadKey();
+// Task № 3
+var a = 3;
+byte b ;
+b = Convert.ToByte(a);
+Console.WriteLine($"Byte b = {b}");
+// Task № 4
+Console.WriteLine("Enter 3 numbers");
+int.TryParse(Console.ReadLine(), out var firstNumber);
+int.TryParse(Console.ReadLine(), out var secondNumber);
+int.TryParse(Console.ReadLine(), out var thirdNumber);
+var average = (firstNumber + secondNumber + thirdNumber) / 3;
+Console.WriteLine($"Average = {average}");
+// Task № 5
+Console.WriteLine("Enter your name");
+var name = Console.ReadLine();
+Console.WriteLine($"Hi {name}");
