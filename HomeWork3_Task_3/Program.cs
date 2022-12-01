a6c0064575792e3ea339a3c9501a55a2254341e8
ShaaaNK <zohanztex@gmail.com>
2022-12-01 10:09:55 +0300

    fix some bugs HM3

--- HomeWork3_Task_3/Program.cs
+++ HomeWork3_Task_3/Program.cs
@@ -1,9 +1,9 @@
 ﻿// bubble sort
 int[] numbers = {1,3,4,3,1,5,6,7,8,9,3,2,1,5,7,9,8,7,6,4,2,1};
 Sort.BubleSort(numbers);
-foreach (var p in numbers)
+foreach (var el in numbers)
 {
-    Console.Write($"{p},");
+    Console.Write($"{el},");
 }
 class Sort
 {
