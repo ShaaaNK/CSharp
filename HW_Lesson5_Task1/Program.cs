d7ddc6c2c9daddda1edef9cf83bfd4f8cbbf7f19
ShaaaNK <zohanztex@gmail.com>
2022-12-12 11:45:13 +0300

    Add HW from Lesson 5

---
+++ HW_Lesson5_Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿void SortArr (int[] array)
+{
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[j+1])
+            {
+                (array[j], array[j + 1]) = (array[j + 1], array[j]);
+            }
+        }
+    }
+}
+
+void AddElementsOfArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Enter {i+1} from {array.Length} numbers");
+        array[i] = int.Parse(Console.ReadLine());
+    }  
+}
+
+int [] arr = new int[6];
+AddElementsOfArray(arr);
+SortArr(arr);
+Console.Write(String.Join(",",arr));
+
---
+++ HW_Lesson_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int [,] arr = new int[3,3] {
+                            {12,2,32},
+                            {32,19,43},
+                            {1,99,33}
+                            };
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    var max = arr[0, i];
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (arr[i,j] > max)
+        {
+            max = arr[i,j];
+            
+        }
+    }
+    Console.WriteLine($"Max element of {i+1} row = {max}");
+}
+
