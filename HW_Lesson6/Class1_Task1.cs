f20533658e049b6238649ea55d2209e8b8baac2e
ShaaaNK <zohanztex@gmail.com>
2022-12-13 15:51:53 +0300

    add comments to HW_Lesson6

--- HW_Lesson6/Class1_Task1.cs
+++ HW_Lesson6/Class1_Task1.cs
@@ -1,6 +1,14 @@
 namespace HW_Lesson6_Task_1;
+/// <summary>
+/// Реверс значений массива
+/// </summary>
 public class ArrayWorker
 {
+    /// <summary>
+    /// Метод для представления элементов массива в обратном порядке
+    /// </summary>
+    /// <param name="arr">Числовой массив </param>
+    /// <returns>Числовой массив</returns>
     public static int [] InvertArr(int[] arr)
     {
         Array.Reverse(arr);
--- HW_Lesson6/Class2_Task2.cs
+++ HW_Lesson6/Class2_Task2.cs
@@ -1,30 +1,44 @@
 namespace HW_Lesson6_Task_1;
-
+/// <summary>
+/// Вычисление геометрических параметров конуса
+/// </summary>
 public class Cone
 {
     const decimal pi = 3.141592M;
     decimal radius;
     decimal height;
-
+    /// <summary>
+    /// Метод определения входных параметров для вычисления площади основания конуса
+    /// </summary>
+    /// <param name="radius">Радиус оснвоания конуса</param>
     public Cone(decimal radius)
     {
         this.radius = radius;
         _ = height;
     }
-
+/// <summary>
+/// Метод определения входных параметров для вычисления полной площади конуса
+/// </summary>
+/// <param name="radius">Радиус основания конуса</param>
+/// <param name="height">Высота конуса</param>
     public Cone(decimal radius, decimal height)
     {
         this.radius = radius;
         this.height = height;
     }
-    
+    /// <summary>
+    /// Вычисление площади основания конуса
+    /// </summary>
     public void CircleSquare()
     {
+        
         var square = pi * (radius * radius);
         Console.WriteLine($"Cone sinking surface area = {square}");
         
     }
-    
+    /// <summary>
+    /// Вычисление полной площади конуса
+    /// </summary>
     public void FullSquare()
     {
         var fullSquare = pi * radius * ((decimal)Math.Sqrt((double)(radius * radius) + (double)(height * height)) + radius);
